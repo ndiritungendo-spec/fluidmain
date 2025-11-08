72323c0578e7058f4dde815145946d1688a61ef7
peterndiritu <peternngendo@gmail.com>
2025-11-08 12:40:46 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 
+const RINKEBY_URL = "https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID";
+const DEPLOYER_PRIVATE_KEY = "YOUR_DEPLOYER_PRIVATE_KEY";
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_URL,
+      accounts: [DEPLOYER_PRIVATE_KEY]
+    },
+    hardhat: {}
+  }
+};
